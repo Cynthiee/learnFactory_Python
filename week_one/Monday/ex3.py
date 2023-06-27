462f946309f413bc2eeb862e83ca274334a7560e
Cynthiee <islacynthia7@gmail.com>
2023-06-27 16:59:07 +0100

    learn python the hard way

---
+++ week_one/Monday/ex3.py
@@ -0,0 +1,24 @@
+# This code prints calculations on chickens.
+print("I will now count my chickens:")
+# Output the no of hens and roasters after calculations.
+print("Hens", 25 + 30 / 6)
+print("Roasters", 100 - 25 * 3 % 4)
+# Output string statement below
+print("Now I will count the eggs:")
+# performs calculations and prints the result
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# prints the string statement below.
+print("Is it true that 3 + 2 < 5 - 7?")
+# performs calculations and prints the result.
+print(3 + 2 < 5 - 7)
+# prints the string, performs calculations and prints result
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+# prints string statement
+print("Oh, that's why it's False.")
+# prints string statement
+print("How about some more.")
+# prints the string, performs calculations and prints result
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ week_one/Monday/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Cynthia C. Ibegbu'
+my_age = 35  # quite a lie
+my_height = 74  # inches
+my_weight = 180  # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, but I'll get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
